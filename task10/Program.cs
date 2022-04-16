832f62ab21feae8557461aba4b790837eff69fde
konpim <kostya-pimenov@mail.ru>
2022-04-17 02:55:48 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Task 10: ");
+int number = 123;
+Reverce(number); //Вызов метода
+
+void Reverce( int currentNumber) //Создание метода
+{
+int a = currentNumber/100;
+int b = currentNumber%100/10;
+int c = currentNumber%10;
+Console.WriteLine(c * 100 + b * 10 + a);
+}
