b41e42a6024b55790782409d2cd085262b2c7c65
konpim <kostya-pimenov@mail.ru>
2022-04-17 02:58:10 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Введите число
+Console.WriteLine("Введите число: ");
+// В пепеменную а сохраняется число из консоли
+string a = Console.ReadLine();
+Third(a);
+// Вызов метода
+
+void Third(string number)
+    {
+        if (number.Length < 3)
+        {
+            Console.WriteLine("Повторите попытку. Введите трехзначное или более число ");
+        }
+        else
+        {
+            Console.WriteLine(number[2]);
+        }
+    }
