486af1edef87eb88facba98fa426cd4e8b7c0db5
KonPim <100049831+Pimenov-Konstantin@users.noreply.github.com>
2022-04-17 03:09:33 +0300

    Add files via upload

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+Console.WriteLine("Введите число 1: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2)
+{
+    Console.WriteLine("Меньшее число " + number1);
+    Console.WriteLine("Большее число " + number2);
+}
+else
+{
+    Console.WriteLine("Меньшее число " + number2);
+    Console.WriteLine("Большее число " + number1);
+}
+
